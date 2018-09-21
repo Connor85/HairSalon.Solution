8869f936312e54d746cab5eb5cdc896d26bd3350
Chan Lee <chanethanlee@gmail.com>
2018-09-21 11:51:22 -0700

    add client controller and few view pages.

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class ClientController : Controller
+  {
+    [HttpGet("stylists/{stylistId}/clients")]
+    public ActionResult Index(int stylistId)
+    {
+      Stylist thisStylist = Stylist.Find(stylistId);
+      List<Client> allClients = thisStylist.GetClients();
+      return View(allClients);
+    }
+
+    [HttpGet("/stylists/{stylistId}/clients/new")]
+    public ActionResult CreateForm(int stylistId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Stylist> allStylists = Stylist.GetAll();
+      Stylist thisStylist = Stylist.Find(stylistId);
+      model.Add("allStylists", allStylists);
+      model.Add("thisStylist", thisStylist);
+      return View(model);
+    }
+
+    [HttpGet("/stylists/{stylistId}/clients/{clientId}")]
+    public ActionResult Details(int stylistId, int clientId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Client thisClient = Client.Find(clientId);
+      Stylist stylist = Stylist.Find(stylistId);
+      model.Add("client", client);
+      model.Add("stylist", stylist);
+      return View(model);
+    }
+
+
+    [HttpGet("/clients/{id}/update")]
+    public ActionResult UpdateForm(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Stylist> allStylists = Stylist.GetAll();
+      Client thisClient = Client.Find(id);
+      model.Add("allStylists", allStylists);
+      model.Add("client", client);
+      return View(model);
+    }
+
+    [HttpPost("/clients/{id}/update")]
+    public ActionResult Update(int id, string newName, string newAppointment, int newStylistId)
+    {
+      Client thisClient = Client.Find(id);
+      thisClient.Edit(newName, newAppointment, newStylistId);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/clients/{id}/delete")]
+    public ActionResult DeleteClient(int id)
+    {
+      Client.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}

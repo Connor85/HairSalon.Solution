758c7873dd7bfbb917c95b6f1d0712ddb44cf26a
Chan Lee <chanethanlee@gmail.com>
2018-09-28 13:06:38 -0700

    update sql files.

--- chan_lee.sql
+++ chan_lee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 21, 2018 at 11:38 PM
+-- Generation Time: Sep 28, 2018 at 08:04 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -24,23 +24,82 @@ USE `chan_lee`;
 
 CREATE TABLE `clients` (
   `id` int(32) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `appointment` varchar(255) NOT NULL,
-  `stylist_id` int(32) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `clients`
 --
 
-INSERT INTO `clients` (`id`, `name`, `appointment`, `stylist_id`) VALUES
-(1, 'Harry Potter', '2018/09/28', 1),
-(2, 'Hermione Granger', '2018/09/29', 1),
-(3, 'Aegon Targaryen', '2018/09/24', 2),
-(4, 'Cersei Lannister', '2018/09/25', 2),
-(5, 'Aria Stark', '2018/09/30', 1),
-(6, 'Captain America', '2019/01/01', 4),
-(7, 'Aria Stark', '2018/09/31', 2);
+INSERT INTO `clients` (`id`, `name`) VALUES
+(1, 'Harry Potter'),
+(2, 'Hermione Granger'),
+(3, 'Aegon Targaryen'),
+(4, 'Cersei Lannister'),
+(5, 'Aria Stark'),
+(6, 'Captain America'),
+(7, 'Aria Stark'),
+(8, 'Ron'),
+(9, 'Jamey Lannister'),
+(10, 'John'),
+(11, 'John'),
+(12, 'John'),
+(13, 'John'),
+(14, 'John'),
+(15, 'John');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialtys`
+--
+
+CREATE TABLE `specialtys` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `specialtys`
+--
+
+INSERT INTO `specialtys` (`id`, `name`) VALUES
+(1, 'Color'),
+(2, 'Highlight'),
+(3, 'Corrective Color'),
+(4, 'Conditioning Texture Treatment'),
+(5, 'Blow Dry & Style'),
+(6, 'Updo'),
+(8, 'John'),
+(9, 'John'),
+(10, 'John'),
+(11, 'John'),
+(12, 'John'),
+(13, 'John');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialtys_stylists`
+--
+
+CREATE TABLE `specialtys_stylists` (
+  `id` int(11) NOT NULL,
+  `specialty_id` int(11) NOT NULL,
+  `stylist_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `specialtys_stylists`
+--
+
+INSERT INTO `specialtys_stylists` (`id`, `specialty_id`, `stylist_id`) VALUES
+(2, 4, 2),
+(3, 4, 6),
+(4, 2, 4),
+(5, 5, 6),
+(6, 6, 2),
+(7, 6, 4);
 
 -- --------------------------------------------------------
 
@@ -50,18 +109,47 @@ INSERT INTO `clients` (`id`, `name`, `appointment`, `stylist_id`) VALUES
 
 CREATE TABLE `stylists` (
   `id` int(32) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `income` int(32) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `stylists`
 --
 
-INSERT INTO `stylists` (`id`, `name`, `income`) VALUES
-(2, 'Chandler', 40000),
-(4, 'Monica', 30000),
-(5, 'Joey', 30000);
+INSERT INTO `stylists` (`id`, `name`) VALUES
+(2, 'Chandler'),
+(4, 'Monica'),
+(5, 'Joey'),
+(6, 'John'),
+(7, 'John'),
+(8, 'John'),
+(9, 'John'),
+(10, 'John'),
+(11, 'John'),
+(12, 'John');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stylists_clients`
+--
+
+CREATE TABLE `stylists_clients` (
+  `id` int(11) NOT NULL,
+  `stylist_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stylists_clients`
+--
+
+INSERT INTO `stylists_clients` (`id`, `stylist_id`, `client_id`) VALUES
+(1, 2, 1),
+(2, 5, 8),
+(3, 6, 8),
+(4, 0, 9),
+(5, 2, 9);
 
 --
 -- Indexes for dumped tables
@@ -74,9 +162,27 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `specialtys`
+--
+ALTER TABLE `specialtys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `specialtys_stylists`
+--
+ALTER TABLE `specialtys_stylists`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `stylists`
 --
 ALTER TABLE `stylists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stylists_clients`
+--
+ALTER TABLE `stylists_clients`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -87,10 +193,28 @@ ALTER TABLE `stylists`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `specialtys`
+--
+ALTER TABLE `specialtys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `specialtys_stylists`
+--
+ALTER TABLE `specialtys_stylists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `stylists_clients`
+--
+ALTER TABLE `stylists_clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
--- chan_lee_test.sql
+++ chan_lee_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 21, 2018 at 11:43 PM
+-- Generation Time: Sep 28, 2018 at 08:05 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -24,10 +24,43 @@ USE `chan_lee_test`;
 
 CREATE TABLE `clients` (
   `id` int(32) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `appointment` varchar(255) NOT NULL,
-  `stylist_id` int(32) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialtys`
+--
+
+CREATE TABLE `specialtys` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialtys_stylists`
+--
+
+CREATE TABLE `specialtys_stylists` (
+  `id` int(11) NOT NULL,
+  `specialty_id` int(11) NOT NULL,
+  `stylist_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `specialtys_stylists`
+--
+
+INSERT INTO `specialtys_stylists` (`id`, `specialty_id`, `stylist_id`) VALUES
+(8, 13, 22),
+(9, 13, 23),
+(10, 19, 32),
+(11, 19, 33),
+(12, 25, 42),
+(13, 25, 43);
 
 -- --------------------------------------------------------
 
@@ -37,9 +70,40 @@ CREATE TABLE `clients` (
 
 CREATE TABLE `stylists` (
   `id` int(32) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `income` int(32) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stylists_clients`
+--
+
+CREATE TABLE `stylists_clients` (
+  `id` int(11) NOT NULL,
+  `stylist_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stylists_clients`
+--
+
+INSERT INTO `stylists_clients` (`id`, `stylist_id`, `client_id`) VALUES
+(6, 19, 21),
+(7, 19, 22),
+(8, 20, 26),
+(9, 21, 26),
+(10, 29, 27),
+(11, 29, 28),
+(12, 30, 32),
+(13, 31, 32),
+(14, 39, 33),
+(15, 39, 34),
+(16, 40, 38),
+(17, 41, 38),
+(18, 49, 39),
+(19, 49, 40);
 
 --
 -- Indexes for dumped tables
@@ -52,9 +116,27 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `specialtys`
+--
+ALTER TABLE `specialtys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `specialtys_stylists`
+--
+ALTER TABLE `specialtys_stylists`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `stylists`
 --
 ALTER TABLE `stylists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stylists_clients`
+--
+ALTER TABLE `stylists_clients`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -65,10 +147,28 @@ ALTER TABLE `stylists`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `specialtys`
+--
+ALTER TABLE `specialtys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `specialtys_stylists`
+--
+ALTER TABLE `specialtys_stylists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `stylists_clients`
+--
+ALTER TABLE `stylists_clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
